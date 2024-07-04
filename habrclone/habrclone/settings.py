049520808029a64c2a760a8e040e8fcd88f26afb
Aren2020 <aren.allahverdyan@gmail.com>
2024-07-04 16:42:49 +0400

    allow all

--- habrclone/habrclone/settings.py
+++ habrclone/habrclone/settings.py
@@ -42,7 +42,7 @@ INSTALLED_APPS = [
     'rest_framework',
     'rest_framework_simplejwt',
     'rest_framework_simplejwt.token_blacklist',
-    # 'corsheaders',
+    'corsheaders',
 
     'users',
 ]
@@ -50,13 +50,15 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
-    # 'corsheaders.middleware.CorsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+
+CORS_ORIGIN_ALLOW_ALL = True 
 
 # CORS_ALLOWED_ORIGINS = [
 #     'http://127.0.0.1:5173',
