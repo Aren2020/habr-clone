ef5936418767e547f587f6299786f9ef685d508a
Aren2020 <aren.allahverdyan@gmail.com>
2024-07-05 15:56:25 +0400

    put request patch in user edit view

--- habrclone/habrclone/settings.py
+++ habrclone/habrclone/settings.py
@@ -58,11 +58,14 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-CORS_ORIGIN_ALLOW_ALL = True 
+# CORS_ORIGIN_ALLOW_ALL = True 
 
-# CORS_ALLOWED_ORIGINS = [
-#     'http://127.0.0.1:5173',
-# ]
+CORS_ALLOWED_ORIGINS = [
+    'http://127.0.0.1:5173',
+]
+
+CORS_ALLOW_CREDENTIALS = True
+
 
 AUTH_USER_MODEL = 'users.User'
 
