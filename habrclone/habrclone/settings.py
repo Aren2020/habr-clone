f79eea52216c97384ebe8df48bfe18d174df827c
Aren2020 <aren.allahverdyan@gmail.com>
2024-07-24 18:09:03 +0400

    remove extra f=g

--- habrclone/habrclone/settings.py
+++ habrclone/habrclone/settings.py
@@ -36,7 +36,7 @@ INSTALLED_APPS = [
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
-    'django.contrib.messages',g
+    'django.contrib.messages',
     'django.contrib.staticfiles',
 
     'rest_framework',
